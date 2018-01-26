1b0683ac88d422b1bbc4606795f0dacbcd138b0e
Hoon Lee <yl.hoony@gmail.com>
2018-01-26 00:38:15 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -25,9 +25,9 @@ class ArtistsController < ApplicationController
   def update
   end
 
-  # private
-  # def artist_params(*args) 
-  #   params.require(:artist).permit(*args) 
-  # end
+  private
+  def artist_params(*args) 
+    params.require(:artist).permit(*args) 
+  end
 
 end
